50ccca75bb33cdc343b794139f072b3232c8004d
udejej <gabriel4545@outlook.fr>
2025-01-17 21:24:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1251209545129132110', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+        footer: 'LILLE', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '3', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
