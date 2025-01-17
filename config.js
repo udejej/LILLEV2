cf5b7ad75ed899cc8cf6f68a3ca6ba36fb7fa5ee
udejej <gabriel4545@outlook.fr>
2025-01-17 21:55:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        buyer: '915321015515643935', // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: 'LILLE', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '3', // Le nombre de serveur maximum que le bot peut rejoindre 
