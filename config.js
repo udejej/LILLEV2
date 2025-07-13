91a4144edb1083899a25763e584d23d25c3e9195
udejej <gabriel4545@outlook.fr>
2025-07-13 12:27:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
+        prefixe: '&', // Préfixe - Le texte avant les commandes
         buyer: '915321015515643935', // Buyer
         couleur: '#6495ED', // Couleur HEX
         footer: 'Lille Interactive', // Dans la commande help ou autre il y'a un texte en bas
